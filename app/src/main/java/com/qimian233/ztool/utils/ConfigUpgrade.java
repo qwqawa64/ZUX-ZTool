964896766dde1c877655bf7eb514973e9e025ae3
WASDDestroy <3288211880@qq.com>
2026-01-08 01:16:45 +0800

    删除读取和写入“配置更新”标记的相关代码，以性能为代价（不存取快速确定持久化存储版本的键值对，因此需要遍历整个SharedPreferences）解决全新安装后莫名弹出配置更新提示的问题

--- app/src/main/java/com/qimian233/ztool/utils/ConfigUpgrade.java
+++ app/src/main/java/com/qimian233/ztool/utils/ConfigUpgrade.java
@@ -20,7 +20,6 @@ public class ConfigUpgrade {
         Log.d(TAG,"All config wiped, start upgrading config");
         // writeConfigToSharedPrefs方法内置了移除module_enabled_前缀的操作，此处直接调用即可。
         mPreferencesUtils.writeConfigToSharedPrefs(allSettings);
-        mPreferencesUtils.saveBooleanSetting("isConfigUpgraded", true);
         Log.d(TAG,"Config upgraded successfully");
     }
 
@@ -28,16 +27,9 @@ public class ConfigUpgrade {
         mPreferencesUtils = new ModulePreferencesUtils(context);
         Map<String,Object> allSettings = mPreferencesUtils.getAllSettings();
         // 如果配置为空，则不需要升级（可能是用户点击了“清除配置”，或者全新安装了APP）
-        // 这个时候可以顺便设置一个配置升级标记，避免重复执行升级操作。
         if (allSettings.isEmpty()){
             Log.d(TAG, "Config is empty, maybe user performed reset or this is a fresh install, skipping upgrade.");
-            mPreferencesUtils.saveBooleanSetting("isConfigUpgraded", true);
             return false;
-        }
-        // 先尝试读取新的配置升级标记，如果没有，则需要升级配置
-        if (!mPreferencesUtils.loadBooleanSetting("isConfigUpgraded", false)) {
-            Log.d(TAG,"Old config format detected, need to upgrade config.");
-            return true;
         }
         // 如果上面的判断没有触发return，则开始逐条遍历配置信息，如果存在module_enabled_前缀，则说明需要升级配置
         for (Map.Entry<String, Object> entry : allSettings.entrySet()) {
