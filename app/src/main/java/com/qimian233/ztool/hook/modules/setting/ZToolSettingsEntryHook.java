3a81cb8a00f1163ad3d3ba389e623ac12ce5d9b2
WASDDestroy <3288211880@qq.com>
2026-07-02 00:58:48 +0800

    patch: fix ztool settings entry hook

--- app/src/main/java/com/qimian233/ztool/hook/modules/setting/ZToolSettingsEntryHook.java
+++ app/src/main/java/com/qimian233/ztool/hook/modules/setting/ZToolSettingsEntryHook.java
@@ -1,5 +1,6 @@
 package com.qimian233.ztool.hook.modules.setting;
 
+import android.annotation.SuppressLint;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -7,12 +8,12 @@ import android.content.Intent;
 import com.qimian233.ztool.MainActivity;
 import com.qimian233.ztool.hook.base.BaseHookModule;
 
-import io.github.libxposed.api.XposedInterface;
 import io.github.libxposed.api.XposedModuleInterface;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
+@SuppressLint("PrivateApi")
 public class ZToolSettingsEntryHook extends BaseHookModule {
     private static final String TARGET_PACKAGE = "com.android.settings";
     private static final String TARGET_CLASS = "com.android.settings.homepage.TopLevelSettings";
@@ -20,7 +21,6 @@ public class ZToolSettingsEntryHook extends BaseHookModule {
     private static final String CATEGORY_KEY = "ztool_settings_category";
     private static final String APP_PACKAGE = "com.qimian233.ztool";
     private static final String ENTRY_TITLE = "ZTool";
-
     public ZToolSettingsEntryHook() {}
 
     @Override
@@ -36,7 +36,6 @@ public class ZToolSettingsEntryHook extends BaseHookModule {
     @Override
     public void handleLoadPackage(XposedModuleInterface.PackageLoadedParam param) throws Throwable {
         ClassLoader classLoader = param.getDefaultClassLoader();
-        String packageName = param.getPackageName();
         try {
             log("Installing hook.");
             Method m = classLoader
@@ -45,21 +44,20 @@ public class ZToolSettingsEntryHook extends BaseHookModule {
             this.xposed.hook(m).intercept(chain -> {
                 Object result = chain.proceed();
                 try {
-                    Method getPrefScreen = chain.getThisObject().getClass()
-                            .getDeclaredMethod("getPreferenceScreen");
+                    Method getPrefScreen = findMethod(chain.getThisObject().getClass(), "getPreferenceScreen");
                     Object screen = getPrefScreen.invoke(chain.getThisObject());
                     if (screen == null) {
                         return result;
                     }
 
-                    Method getContext = screen.getClass().getDeclaredMethod("getContext");
+                    Method getContext = findMethod(screen.getClass(), "getContext");
                     Context context = (Context) getContext.invoke(screen);
                     if (context == null) {
                         return result;
                     }
 
-                    Method findPreference = screen.getClass()
-                            .getDeclaredMethod("findPreference", CharSequence.class);
+                    Method findPreference = findMethod(screen.getClass(),
+                            "findPreference", CharSequence.class);
                     if (findPreference.invoke(screen, ENTRY_KEY) != null) {
                         return result;
                     }
@@ -72,15 +70,15 @@ public class ZToolSettingsEntryHook extends BaseHookModule {
                     Constructor<?> categoryCtor = preferenceCategoryClass
                             .getDeclaredConstructor(Context.class);
                     Object category = categoryCtor.newInstance(context);
-                    Method setKey = preferenceCategoryClass.getDeclaredMethod("setKey", String.class);
+                    Method setKey = findMethod(preferenceCategoryClass, "setKey", String.class);
                     setKey.invoke(category, CATEGORY_KEY);
-                    Method setOrder = preferenceCategoryClass.getDeclaredMethod("setOrder", int.class);
+                    Method setOrder = findMethod(preferenceCategoryClass, "setOrder", int.class);
                     setOrder.invoke(category, -90);
 
                     Constructor<?> prefCtor = preferenceClass.getDeclaredConstructor(Context.class);
                     Object entry = prefCtor.newInstance(context);
                     setKey.invoke(entry, ENTRY_KEY);
-                    Method setTitle = preferenceClass.getDeclaredMethod("setTitle", CharSequence.class);
+                    Method setTitle = findMethod(preferenceClass, "setTitle", CharSequence.class);
                     setTitle.invoke(entry, ENTRY_TITLE);
                     setOrder.invoke(entry, Integer.MIN_VALUE + 1);
 
@@ -94,8 +92,7 @@ public class ZToolSettingsEntryHook extends BaseHookModule {
                     Method setIcon = preferenceClass.getDeclaredMethod("setIcon", android.graphics.drawable.Drawable.class);
                     setIcon.invoke(entry, context.getPackageManager().getApplicationIcon(APP_PACKAGE));
 
-                    Method addPreference = screen.getClass()
-                            .getDeclaredMethod("addPreference",
+                    Method addPreference = findMethod(screen.getClass(), "addPreference",
                                     classLoader.loadClass("androidx.preference.Preference"));
                     addPreference.invoke(screen, category);
                     addPreference.invoke(category, entry);
