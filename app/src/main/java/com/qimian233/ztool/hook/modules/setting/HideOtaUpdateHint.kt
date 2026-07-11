d83e771e5c3a27729e246e84eb9209399f64efbf
WASDDestroy <3288211880@qq.com>
2026-07-11 13:56:25 +0800

    hook: Kotlinize HideOtaUpdateHint and add interceptor to block new version notification

--- app/src/main/java/com/qimian233/ztool/hook/modules/setting/HideOtaUpdateHint.kt
+++ app/src/main/java/com/qimian233/ztool/hook/modules/setting/HideOtaUpdateHint.kt
@@ -1,62 +1,74 @@
-package com.qimian233.ztool.hook.modules.setting;
+package com.qimian233.ztool.hook.modules.setting
 
-import android.content.ContentResolver;
-import android.provider.Settings;
-
-import com.qimian233.ztool.hook.base.BaseHookModule;
-
-import io.github.libxposed.api.XposedInterface;
-import io.github.libxposed.api.XposedModuleInterface;
-
-import java.lang.reflect.Method;
+import android.content.ContentResolver
+import android.provider.Settings.Secure
+import com.qimian233.ztool.hook.base.BaseHookModule
+import io.github.libxposed.api.XposedModuleInterface.PackageLoadedParam
+import java.lang.reflect.Method
 
 /**
  * Hides the red OTA update hint in Settings while keeping the OTA entry usable.
  */
-public class HideOtaUpdateHint extends BaseHookModule {
-    private static final String TARGET_PACKAGE = "com.android.settings";
-    private static final String OTA_NEW_VERSION_FOUND = "lenovo_ota_new_version_found";
+class HideOtaUpdateHint : BaseHookModule() {
+    override fun getModuleName(): String = "hide_ota_update_hint"
 
-    public HideOtaUpdateHint() {}
+    override fun getTargetPackages(): Array<String> = arrayOf(TARGET_PACKAGE)
 
-    @Override
-    public String getModuleName() {
-        return "hide_ota_update_hint";
+    @Throws(Throwable::class)
+    override fun handleLoadPackage(param: PackageLoadedParam) {
+        handlePreferenceRead()
+        handleNotificationCreate(param)
     }
 
-    @Override
-    public String[] getTargetPackages() {
-        return new String[]{TARGET_PACKAGE};
-    }
-
-    @Override
-    public void handleLoadPackage(XposedModuleInterface.PackageLoadedParam param) throws Throwable {
-        ClassLoader classLoader = param.getDefaultClassLoader();
-        String packageName = param.getPackageName();
+    fun handlePreferenceRead() {
         try {
             // getInt(ContentResolver, String, int)
-            Method getInt3 = Settings.Secure.class.getDeclaredMethod(
-                    "getInt", ContentResolver.class, String.class, int.class);
-            this.xposed.hook(getInt3).intercept(chain -> {
-                if (OTA_NEW_VERSION_FOUND.equals(chain.getArg(1))) {
-                    return 0;
+            val getInt3 : Method = Secure::class.java.getDeclaredMethod(
+                "getInt",
+                ContentResolver::class.java,
+                String::class.java,
+                Int::class.javaPrimitiveType
+            )
+            this.xposed.hook(getInt3).intercept { chain ->
+                if (OTA_NEW_VERSION_FOUND == chain.getArg(1)) {
+                    return@intercept 0
                 }
-                return chain.proceed();
-            });
+                chain.proceed()
+            }
 
             // getInt(ContentResolver, String)
-            Method getInt2 = Settings.Secure.class.getDeclaredMethod(
-                    "getInt", ContentResolver.class, String.class);
-            this.xposed.hook(getInt2).intercept(chain -> {
-                if (OTA_NEW_VERSION_FOUND.equals(chain.getArg(1))) {
-                    return 0;
+            val getInt2 = Secure::class.java.getDeclaredMethod(
+                "getInt", ContentResolver::class.java, String::class.java
+            )
+            this.xposed.hook(getInt2).intercept { chain ->
+                if (OTA_NEW_VERSION_FOUND == chain.getArg(1)) {
+                    return@intercept 0
                 }
-                return chain.proceed();
-            });
+                chain.proceed()
+            }
 
-            log("Hooked Settings OTA new-version flag reads");
-        } catch (Throwable t) {
-            logError("Failed to hook Settings OTA new-version flag reads", t);
+            log("Hooked Settings OTA new-version flag reads")
+        } catch (t: Throwable) {
+            logError("Failed to hook Settings OTA new-version flag reads", t)
         }
+    }
+
+    fun handleNotificationCreate(param: PackageLoadedParam) {
+        try {
+            log("Hooking OTA notification create method")
+            val cl: ClassLoader = param.defaultClassLoader
+            val targetClass: Class<*> = cl.loadClass("com.lenovo.row.ota.core.d.notification.NotificationCenter")
+            val targetMethod: Method = findMethod(targetClass, "showNewVersionNotification")
+            xposed.hook(targetMethod).intercept { _ ->
+                return@intercept null
+            }
+        } catch (th: Throwable) {
+            logError("Failed to hook OTA update notification creation method!", th)
+        }
+    }
+
+    companion object {
+        private const val TARGET_PACKAGE = "com.android.settings"
+        private const val OTA_NEW_VERSION_FOUND = "lenovo_ota_new_version_found"
     }
 }
