ea0b10796504c7ea744728b963c11e84544e1ab0
WASDDestroy <3288211880@qq.com>
2026-07-31 12:19:51 +0800

    hook: use correct hook to disable recent app display in task bar

--- app/src/main/java/com/qimian233/ztool/hook/modules/launcher/dockbar/DisableRecentAppsDisplay.kt
+++ app/src/main/java/com/qimian233/ztool/hook/modules/launcher/dockbar/DisableRecentAppsDisplay.kt
@@ -1,20 +1,34 @@
 package com.qimian233.ztool.hook.modules.launcher.dockbar
 
 import android.annotation.SuppressLint
+import android.content.Context
 import com.qimian233.ztool.hook.base.BaseHookModule
 import io.github.libxposed.api.XposedModuleInterface
 import java.lang.reflect.Method
 
 @SuppressLint("PrivateApi")
 class DisableRecentAppsDisplay: BaseHookModule() {
-    override fun getModuleName(): String = "test_hook"
+    override fun getModuleName(): String = "disable_recent_apps_display"
 
     override fun getTargetPackages(): Array<out String> = arrayOf("com.zui.launcher")
 
     override fun handleLoadPackage(param: XposedModuleInterface.PackageLoadedParam) {
         val classLoader: ClassLoader = param.defaultClassLoader
-        val featureEvaluatorClass: Class<*> = classLoader.loadClass("com.android.launcher3.taskbar.customization.TaskbarFeatureEvaluator")
-        val recentsEnabledGetter: Method = findMethod(featureEvaluatorClass, "isRecentsEnabled")
-        hookWithId(recentsEnabledGetter, "disable_recents") { return@hookWithId false }
+        // Hook 1: handles Android launcher3 taskbar
+        val utilsClass: Class<*> = classLoader.loadClass("com.android.launcher3.taskbar.Utilities")
+        val tbRecentAppEnableGetter: Method = findMethod(utilsClass, "isTbRecentUsedAppEnable",
+            Context::class.java)
+        hookWithId(tbRecentAppEnableGetter, "hook_utils") {
+            return@hookWithId false
+        }
+
+        // Hook 2: handles static method setRecentUsedShow in com.zui.launcher.uiextend.ZuiHotseat
+        val hotSeatClass: Class<*> = classLoader.loadClass("com.zui.launcher.uiextend.ZuiHotseat")
+        val recentShowSetter: Method = findMethod(hotSeatClass, "setRecentUsedShow", Boolean::class.java)
+        hookWithId(recentShowSetter, "hook_recent_show_setter") { chain ->
+            val argList = chain.args.toMutableList()
+            argList[0] = false
+            return@hookWithId chain.proceed(argList.toTypedArray())
+        }
     }
 }
