7a3ce4d63bc228809cd7496879144e8812e0fde2
WASDDestroy <3288211880@qq.com>
2026-08-05 23:28:13 +0800

    refactor: convert OneVisionCompletion.java to Kotlin

--- app/src/main/java/com/qimian233/ztool/hook/modules/setting/OneVisionCompletion.kt
+++ app/src/main/java/com/qimian233/ztool/hook/modules/setting/OneVisionCompletion.kt
@@ -1,58 +1,34 @@
-package com.qimian233.ztool.hook.modules.setting;
+package com.qimian233.ztool.hook.modules.setting
 
-import com.qimian233.ztool.hook.base.AppHookModule;
-
-import io.github.libxposed.api.XposedModuleInterface;
-
-import java.lang.reflect.Method;
+import com.qimian233.ztool.hook.base.AppHookModule
+import io.github.libxposed.api.XposedModuleInterface.PackageLoadedParam
 
 /**
  * 系统设置Hook模块
  * 修改系统设置应用的行为
  */
-public class OneVisionCompletion extends AppHookModule {
+class OneVisionCompletion : AppHookModule() {
+    override fun getModuleName(): String = "remove_blacklist"
 
-    public OneVisionCompletion() {}
+    override fun getTargetPackages(): Array<String> = arrayOf("com.android.settings")
 
-    @Override
-    public String getModuleName() {
-        return "remove_blacklist";
+    override fun handleLoadPackage(param: PackageLoadedParam) {
+        val classLoader = param.defaultClassLoader
+        hookSettingsAppManager(classLoader)
     }
 
-    @Override
-    public String[] getTargetPackages() {
-        return new String[]{"com.android.settings", "com.lenovo.settings"};
-    }
-
-    @Override
-    public void handleLoadPackage(XposedModuleInterface.PackageLoadedParam param) throws Throwable {
-        ClassLoader classLoader = param.getDefaultClassLoader();
-        String packageName = param.getPackageName();
-        if ("com.android.settings".equals(packageName)) {
-            hookSettingsAppManager(classLoader);
-        } else if ("com.lenovo.settings".equals(packageName)) {
-            hookLenovoSettings();
-        }
-    }
-
-    private void hookSettingsAppManager(ClassLoader classLoader) {
+    private fun hookSettingsAppManager(classLoader: ClassLoader) {
         try {
-            Method m = classLoader
-                    .loadClass("com.lenovo.settings.onevision.horizontal.SettingsEmbeddingAppManager")
-                    .getDeclaredMethod("getZuiLandScapeShouldBeHideAppList");
-            hookWithId(m, "hook_44", chain -> new String[0]);
-            logger.info("Successfully hooked SettingsEmbeddingAppManager");
-        } catch (Throwable t) {
-            logger.error("Failed to hook SettingsEmbeddingAppManager", t);
-        }
-    }
-
-    private void hookLenovoSettings() {
-        try {
-            // 这里可以添加更多Lenovo设置的Hook
-            logger.debug("Lenovo settings hook placeholder");
-        } catch (Throwable t) {
-            logger.error("Failed to hook Lenovo settings", t);
+            val m = classLoader
+                .loadClass("com.lenovo.settings.onevision.horizontal.SettingsEmbeddingAppManager")
+                .getDeclaredMethod("getZuiLandScapeShouldBeHideAppList")
+            hookWithId(
+                m,
+                "hook_44"
+            ) { arrayOfNulls<String>(0) }
+            logger.info("Successfully hooked SettingsEmbeddingAppManager")
+        } catch (t: Throwable) {
+            logger.error("Failed to hook SettingsEmbeddingAppManager", t)
         }
     }
 }
