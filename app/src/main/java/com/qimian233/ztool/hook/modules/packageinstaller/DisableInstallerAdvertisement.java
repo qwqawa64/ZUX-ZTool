b0a669dd9f217e2b89ff8c5618db77ca53318318
WASDDestroy <3288211880@qq.com>
2026-08-08 16:20:43 +0800

    chore: remove google installer package name in DisableInstallerAdvertisement.java

--- app/src/main/java/com/qimian233/ztool/hook/modules/packageinstaller/DisableInstallerAdvertisement.java
+++ app/src/main/java/com/qimian233/ztool/hook/modules/packageinstaller/DisableInstallerAdvertisement.java
@@ -1,5 +1,7 @@
 package com.qimian233.ztool.hook.modules.packageinstaller;
 
+import android.annotation.SuppressLint;
+
 import com.qimian233.ztool.data.ScopeKeys;
 import com.qimian233.ztool.hook.base.AppHookModule;
 
@@ -11,6 +13,7 @@ import java.lang.reflect.Method;
  * 禁用PackageInstaller应用安装完成后的推荐广告Hook模块
  * 功能：阻止安装成功页面初始化推荐应用数据，消除广告干扰
  */
+@SuppressLint("PrivateApi")
 public class DisableInstallerAdvertisement extends AppHookModule {
 
     public DisableInstallerAdvertisement() {}
@@ -24,19 +27,13 @@ public class DisableInstallerAdvertisement extends AppHookModule {
     public String[] getTargetPackages() {
         return new String[]{
                 ScopeKeys.PACKAGE_INSTALLER.packageName,
-                ScopeKeys.GOOGLE_PACKAGE_INSTALLER.packageName  // 部分设备可能使用Google的包名
         };
     }
 
     @Override
     public void handleLoadPackage(XposedModuleInterface.PackageLoadedParam param) throws Throwable {
         ClassLoader classLoader = param.getDefaultClassLoader();
-        String packageName = param.getPackageName();
-        if (ScopeKeys.PACKAGE_INSTALLER.packageName.equals(packageName)) {
-            hookAndroidPackageInstaller(classLoader);
-        } else if (ScopeKeys.GOOGLE_PACKAGE_INSTALLER.packageName.equals(packageName)) {
-            hookGooglePackageInstaller(classLoader);
-        }
+        hookAndroidPackageInstaller(classLoader);
     }
 
     private void hookAndroidPackageInstaller(ClassLoader classLoader) {
@@ -56,37 +53,6 @@ public class DisableInstallerAdvertisement extends AppHookModule {
 
         } catch (Throwable t) {
             logger.error("Hook PackageInstaller失败", t);
-        }
-    }
-
-    private void hookGooglePackageInstaller(ClassLoader classLoader) {
-        try {
-            // Google版本的PackageInstaller可能有不同的类结构
-            // 这里可以添加对Google版本的特殊处理
-            logger.warn("检测到Google PackageInstaller，使用标准Hook方法");
-
-            // 尝试Hook相同的类和方法
-            Class<?> installSuccessClass = null;
-            try {
-                installSuccessClass = classLoader.loadClass(
-                        "com.android.packageinstaller.InstallSuccessExtra");
-            } catch (ClassNotFoundException e) {
-                // class not found, installSuccessClass remains null
-            }
-
-            if (installSuccessClass != null) {
-                Method initRecommendAppsData = installSuccessClass.getDeclaredMethod("initRecommendAppsData");
-                hookWithId(initRecommendAppsData, "init_recommend_apps_data_2", chain -> {
-                    logger.debug("已阻止Google PackageInstaller广告数据初始化");
-                    return null;
-                });
-                logger.info("成功Hook Google PackageInstaller广告屏蔽");
-            } else {
-                logger.warn("Google PackageInstaller未找到目标类，可能需要适配");
-            }
-
-        } catch (Throwable t) {
-            logger.error("Hook Google PackageInstaller失败", t);
         }
     }
 }
