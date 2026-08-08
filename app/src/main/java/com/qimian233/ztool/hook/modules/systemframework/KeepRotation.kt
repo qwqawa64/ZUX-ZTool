5b4712d9309b4e5c4391aad3ab07f63b95847f90
WASDDestroy <3288211880@qq.com>
2026-08-08 17:35:08 +0800

    refactor: convert KeepRotation.java to Kotlin

--- app/src/main/java/com/qimian233/ztool/hook/modules/systemframework/KeepRotation.kt
+++ app/src/main/java/com/qimian233/ztool/hook/modules/systemframework/KeepRotation.kt
@@ -1,32 +1,30 @@
-package com.qimian233.ztool.hook.modules.systemframework;
+package com.qimian233.ztool.hook.modules.systemframework
 
-import com.qimian233.ztool.data.keys.ScopeKeys;
-import com.qimian233.ztool.hook.base.SystemHookModule;
+import com.qimian233.ztool.data.keys.PreferenceKeys
+import com.qimian233.ztool.data.keys.ScopeKeys
+import com.qimian233.ztool.hook.base.SystemHookModule
+import io.github.libxposed.api.XposedModuleInterface.SystemServerStartingParam
+import java.lang.Boolean
+import kotlin.Array
+import kotlin.String
+import kotlin.Throwable
+import kotlin.arrayOf
 
-import io.github.libxposed.api.XposedModuleInterface;
+class KeepRotation : SystemHookModule() {
+    override fun getModuleName(): String = PreferenceKeys.KEEP_ROTATION.name
 
-import java.lang.reflect.Method;
+    override fun getTargetPackages(): Array<String> = arrayOf(ScopeKeys.SYSTEM_SERVER.packageName)
 
-public class KeepRotation extends SystemHookModule {
-    public static final String FEATURE_NAME = "keep_rotation";
-    public static final String TARGET_PACKAGE = ScopeKeys.SYSTEM_SERVER.packageName;
-
-    public KeepRotation() {}
-
-    public String getModuleName() { return FEATURE_NAME; }
-    public String[] getTargetPackages() { return new String[]{TARGET_PACKAGE};}
-
-    @Override
-    public void handleSystemServerStarting(XposedModuleInterface.SystemServerStartingParam param) throws Throwable {
-        ClassLoader classLoader = param.getClassLoader();
-        logger.info("Hooking DisplayRotation.isRotationCts");
-        try{
-            Method method = classLoader.loadClass("com.zui.server.wm.ZuiDisplayRotation")
-                    .getDeclaredMethod("isRotationCts");
-            hookWithId(method, "is_rotation_cts", chain -> Boolean.TRUE);
-            logger.info("Hooked DisplayRotation.isRotationCts [OK]");
-        } catch (Exception e) {
-            logger.error("Error hooking DisplayRotation.isRotationCts", e);
+    override fun handleSystemServerStarting(param: SystemServerStartingParam) {
+        val classLoader = param.classLoader
+        logger.info("Hooking DisplayRotation.isRotationCts")
+        try {
+            val method = classLoader.loadClass("com.zui.server.wm.ZuiDisplayRotation")
+                .getDeclaredMethod("isRotationCts")
+            hookWithId(method, "is_rotation_cts") { Boolean.TRUE }
+            logger.info("Hooked DisplayRotation.isRotationCts [OK]")
+        } catch (th: Throwable) {
+            logger.error("Error hooking DisplayRotation.isRotationCts", th)
         }
     }
 }
