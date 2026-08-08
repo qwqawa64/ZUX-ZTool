b16e055a13de849a78e2d191b364f461545d13cc
WASDDestroy <3288211880@qq.com>
2026-08-08 17:31:50 +0800

    refactor: convert DisableFlagSecure.java to Kotlin

--- app/src/main/java/com/qimian233/ztool/hook/modules/systemframework/DisableFlagSecure.kt
+++ app/src/main/java/com/qimian233/ztool/hook/modules/systemframework/DisableFlagSecure.kt
@@ -1,54 +1,33 @@
-package com.qimian233.ztool.hook.modules.systemframework;
+package com.qimian233.ztool.hook.modules.systemframework
 
-import android.annotation.SuppressLint;
-
-import com.qimian233.ztool.data.keys.ScopeKeys;
-import com.qimian233.ztool.hook.base.SystemHookModule;
-
-import io.github.libxposed.api.XposedModuleInterface;
-
-import java.lang.reflect.Method;
+import android.annotation.SuppressLint
+import com.qimian233.ztool.data.keys.PreferenceKeys
+import com.qimian233.ztool.data.keys.ScopeKeys
+import com.qimian233.ztool.hook.base.SystemHookModule
+import io.github.libxposed.api.XposedModuleInterface.SystemServerStartingParam
 
 /**
  * 禁用FLAG_SECURE标志Hook模块
  * 作用：移除安全窗口标志，允许对"安全内容"进行截图
  */
-@SuppressLint({"PrivateApi"})
-public class DisableFlagSecure extends SystemHookModule {
+@SuppressLint("PrivateApi")
+class DisableFlagSecure : SystemHookModule() {
+    override fun getModuleName(): String = PreferenceKeys.DISABLE_FLAG_SECURE.name
 
-    public DisableFlagSecure() {}
+    override fun getTargetPackages(): Array<String> = arrayOf(ScopeKeys.SYSTEM_SERVER.packageName)
 
-    @Override
-    public String getModuleName() {
-        return "disable_flag_secure";
-    }
-
-    @Override
-    public String[] getTargetPackages() {
-        return new String[]{ScopeKeys.SYSTEM_SERVER.packageName};
-    }
-
-    @Override
-    public void handleSystemServerStarting(XposedModuleInterface.SystemServerStartingParam param) {
-        ClassLoader classLoader = param.getClassLoader();
-        hookAndroidSystem(classLoader);
-    }
-
-    private void hookAndroidSystem(ClassLoader classLoader) {
+    override fun handleSystemServerStarting(param: SystemServerStartingParam) {
+        val classLoader = param.classLoader
         try {
-            logger.info("开始Hook FLAG_SECURE...");
-            Class<?> windowStateClass = classLoader.loadClass(
-                    "com.android.server.wm.WindowState"
-            );
-
-            // Hook isSecureLocked方法，始终返回false
-            Method method = windowStateClass.getDeclaredMethod("isSecureLocked");
-            hookWithId(method, "is_secure_locked", chain -> false);
-
-            logger.info("成功Hook WindowState.isSecureLocked()");
-
-        } catch (Throwable t) {
-            logger.error("Hook FLAG_SECURE失败", t);
+            logger.info("开始Hook FLAG_SECURE...")
+            val windowStateClass = classLoader.loadClass(
+                "com.android.server.wm.WindowState"
+            )
+            val method = windowStateClass.getDeclaredMethod("isSecureLocked")
+            hookWithId(method, "is_secure_locked") { false }
+            logger.info("成功Hook WindowState.isSecureLocked()")
+        } catch (t: Throwable) {
+            logger.error("Hook FLAG_SECURE失败", t)
         }
     }
 }
