bd0623647326681c741b9fefbcf16a2b24e60e4e
WASDDestroy <3288211880@qq.com>
2026-08-08 16:27:53 +0800

    refactor: convert DisableInstallerAdvertisement.java to Kotlin

--- app/src/main/java/com/qimian233/ztool/hook/modules/packageinstaller/DisableInstallerAdvertisement.kt
+++ app/src/main/java/com/qimian233/ztool/hook/modules/packageinstaller/DisableInstallerAdvertisement.kt
@@ -1,58 +1,43 @@
-package com.qimian233.ztool.hook.modules.packageinstaller;
+package com.qimian233.ztool.hook.modules.packageinstaller
 
-import android.annotation.SuppressLint;
-
-import com.qimian233.ztool.data.ScopeKeys;
-import com.qimian233.ztool.hook.base.AppHookModule;
-
-import io.github.libxposed.api.XposedModuleInterface;
-
-import java.lang.reflect.Method;
+import android.annotation.SuppressLint
+import com.qimian233.ztool.data.PreferenceKeys
+import com.qimian233.ztool.data.ScopeKeys
+import com.qimian233.ztool.hook.base.AppHookModule
+import io.github.libxposed.api.XposedModuleInterface.PackageLoadedParam
 
 /**
  * 禁用PackageInstaller应用安装完成后的推荐广告Hook模块
  * 功能：阻止安装成功页面初始化推荐应用数据，消除广告干扰
  */
 @SuppressLint("PrivateApi")
-public class DisableInstallerAdvertisement extends AppHookModule {
+class DisableInstallerAdvertisement : AppHookModule() {
+    override fun getModuleName(): String = PreferenceKeys.DISABLE_INSTALLER_AD.name
 
-    public DisableInstallerAdvertisement() {}
+    override fun getTargetPackages(): Array<String> = arrayOf(ScopeKeys.PACKAGE_INSTALLER.packageName)
 
-    @Override
-    public String getModuleName() {
-        return "disable_installerAD";
-    }
-
-    @Override
-    public String[] getTargetPackages() {
-        return new String[]{
-                ScopeKeys.PACKAGE_INSTALLER.packageName,
-        };
-    }
-
-    @Override
-    public void handleLoadPackage(XposedModuleInterface.PackageLoadedParam param) throws Throwable {
-        ClassLoader classLoader = param.getDefaultClassLoader();
-        hookAndroidPackageInstaller(classLoader);
-    }
-
-    private void hookAndroidPackageInstaller(ClassLoader classLoader) {
+    override fun handleLoadPackage(param: PackageLoadedParam) {
+        val classLoader = param.defaultClassLoader
         try {
-            Class<?> installSuccessClass = classLoader.loadClass(
-                    "com.android.packageinstaller.InstallSuccessExtra");
+            val installSuccessClass = classLoader.loadClass(
+                "com.android.packageinstaller.InstallSuccessExtra"
+            )
 
             // Hook initRecommendAppsData方法，阻止广告数据初始化
-            Method initRecommendAppsData = installSuccessClass.getDeclaredMethod("initRecommendAppsData");
-            hookWithId(initRecommendAppsData, "init_recommend_apps_data_1", chain -> {
+            val initRecommendAppsData =
+                installSuccessClass.getDeclaredMethod("initRecommendAppsData")
+            hookWithId(
+                initRecommendAppsData,
+                "init_recommend_apps_data_1"
+            ) {
                 // 直接返回，不执行任何广告初始化逻辑
-                logger.debug("已阻止PackageInstaller广告数据初始化");
-                return null;
-            });
+                logger.debug("已阻止PackageInstaller广告数据初始化")
+                null
+            }
 
-            logger.info("成功Hook PackageInstaller广告屏蔽模块");
-
-        } catch (Throwable t) {
-            logger.error("Hook PackageInstaller失败", t);
+            logger.info("成功Hook PackageInstaller广告屏蔽模块")
+        } catch (t: Throwable) {
+            logger.error("Hook PackageInstaller失败", t)
         }
     }
 }
