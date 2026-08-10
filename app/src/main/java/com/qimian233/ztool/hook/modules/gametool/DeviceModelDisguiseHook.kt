53078b50c8f7caf343b960dade365f831300ded4
WASDDestroy <3288211880@qq.com>
2026-08-10 12:31:02 +0800

    refactor: convert DeviceModelDisguiseHook.java to Kotlin

--- app/src/main/java/com/qimian233/ztool/hook/modules/gametool/DeviceModelDisguiseHook.kt
+++ app/src/main/java/com/qimian233/ztool/hook/modules/gametool/DeviceModelDisguiseHook.kt
@@ -1,54 +1,39 @@
-package com.qimian233.ztool.hook.modules.gametool;
+package com.qimian233.ztool.hook.modules.gametool
 
-import com.qimian233.ztool.data.keys.ScopeKeys;
-import com.qimian233.ztool.hook.base.AppHookModule;
-
-import io.github.libxposed.api.XposedModuleInterface;
-
-import java.lang.reflect.Method;
+import com.qimian233.ztool.data.keys.PreferenceKeys
+import com.qimian233.ztool.data.keys.ScopeKeys
+import com.qimian233.ztool.hook.base.AppHookModule
+import io.github.libxposed.api.XposedModuleInterface.PackageLoadedParam
 
 /**
  * 游戏服务设备型号伪装Hook模块
  * 将设备型号伪装为TB322FC，用于绕过游戏服务的设备检测
  */
-public class DeviceModelDisguiseHook extends AppHookModule {
+class DeviceModelDisguiseHook : AppHookModule() {
+    override fun getModuleName(): String = PreferenceKeys.DISGUISE_TB322FC.name
 
-    public DeviceModelDisguiseHook() {}
+    override fun getTargetPackages(): Array<String> = arrayOf(ScopeKeys.GAME_SERVICE.packageName)
 
-    @Override
-    public String getModuleName() {
-        return "disguise_TB322FC";
+    override fun handleLoadPackage(param: PackageLoadedParam) {
+        val classLoader = param.defaultClassLoader
+        hookDeviceUtils(classLoader)
     }
 
-    @Override
-    public String[] getTargetPackages() {
-        return new String[]{
-                ScopeKeys.GAME_SERVICE.packageName
-        };
-    }
-
-    @Override
-    public void handleLoadPackage(XposedModuleInterface.PackageLoadedParam param) throws Throwable {
-        ClassLoader classLoader = param.getDefaultClassLoader();
-        String packageName = param.getPackageName();
-        if (ScopeKeys.GAME_SERVICE.packageName.equals(packageName)) {
-            hookDeviceUtils(classLoader);
-        }
-    }
-
-    private void hookDeviceUtils(ClassLoader classLoader) {
+    private fun hookDeviceUtils(classLoader: ClassLoader) {
         try {
             // 查找DeviceUtils类
-            Class<?> deviceUtilsClass = classLoader.loadClass("com.zui.util.DeviceUtils");
+            val deviceUtilsClass = classLoader.loadClass("com.zui.util.DeviceUtils")
 
             // Hook getBuildModel方法，强制返回目标型号
-            Method getBuildModelMethod = deviceUtilsClass.getDeclaredMethod("getBuildModel");
-            hookWithId(getBuildModelMethod, "get_build_model", chain -> "TB322FC");
+            val getBuildModelMethod = deviceUtilsClass.getDeclaredMethod("getBuildModel")
+            hookWithId(
+                getBuildModelMethod,
+                "get_build_model"
+            ) { "TB322FC" }
 
-            logger.info("Successfully hooked DeviceUtils.getBuildModel for com.zui.game.service");
-
-        } catch (Exception e) {
-            logger.error("Failed to hook DeviceUtils.getBuildModel", e);
+            logger.info("Successfully hooked DeviceUtils.getBuildModel for com.zui.game.service")
+        } catch (e: Exception) {
+            logger.error("Failed to hook DeviceUtils.getBuildModel", e)
         }
     }
 }
