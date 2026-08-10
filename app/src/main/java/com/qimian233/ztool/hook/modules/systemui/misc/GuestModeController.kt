dbbb76f50a92f09c13e87d98d9d613d23bd2099d
WASDDestroy <3288211880@qq.com>
2026-08-10 19:48:00 +0800

    refactor: convert GuestModeController.java to Kotlin

--- app/src/main/java/com/qimian233/ztool/hook/modules/systemui/misc/GuestModeController.kt
+++ app/src/main/java/com/qimian233/ztool/hook/modules/systemui/misc/GuestModeController.kt
@@ -1,74 +1,56 @@
-package com.qimian233.ztool.hook.modules.systemui.misc;
+package com.qimian233.ztool.hook.modules.systemui.misc
 
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.provider.Settings;
-
-import com.qimian233.ztool.data.keys.ScopeKeys;
-import com.qimian233.ztool.hook.base.AppHookModule;
-
-import io.github.libxposed.api.XposedModuleInterface;
-
-import java.lang.reflect.Method;
+import android.annotation.SuppressLint
+import android.content.Context
+import android.provider.Settings
+import com.qimian233.ztool.data.keys.PreferenceKeys
+import com.qimian233.ztool.data.keys.ScopeKeys
+import com.qimian233.ztool.hook.base.AppHookModule
+import io.github.libxposed.api.XposedModuleInterface.PackageLoadedParam
 
 /**
  * 访客模式控制Hook模块
  * 修复系统UI中自动创建访客用户的逻辑
  * 当用户切换器被禁用时，阻止自动添加访客用户
  */
-public class GuestModeController extends AppHookModule {
+class GuestModeController : AppHookModule() {
+    override fun getModuleName(): String = PreferenceKeys.GUEST_MODE_CONTROLLER.name
 
-    public GuestModeController() {}
+    override fun getTargetPackages(): Array<String> = arrayOf(ScopeKeys.SYSTEM_UI.packageName)
 
-    @Override
-    public String getModuleName() {
-        return "guest_mode_controller";
+    override fun handleLoadPackage(param: PackageLoadedParam) {
+        val classLoader = param.defaultClassLoader
+        hookGuestUserInteractor(classLoader)
     }
 
-    @Override
-    public String[] getTargetPackages() {
-        return new String[]{
-                ScopeKeys.SYSTEM_UI.packageName
-        };
-    }
-
-    @Override
-    public void handleLoadPackage(XposedModuleInterface.PackageLoadedParam param) throws Throwable {
-        ClassLoader classLoader = param.getDefaultClassLoader();
-        String packageName = param.getPackageName();
-        if (ScopeKeys.SYSTEM_UI.packageName.equals(packageName)) {
-            hookGuestUserInteractor(classLoader);
-        }
-    }
-
-    private void hookGuestUserInteractor(ClassLoader classLoader) {
+    private fun hookGuestUserInteractor(classLoader: ClassLoader) {
         try {
-            @SuppressLint("PrivateApi") Method isAllowedMethod = classLoader
-                    .loadClass("com.android.systemui.user.domain.interactor.GuestUserInteractor")
-                    .getDeclaredMethod("isDeviceAllowedToAddGuest");
-            hookWithId(isAllowedMethod, "is_allowed", chain -> {
+            @SuppressLint("PrivateApi") val isAllowedMethod = classLoader
+                .loadClass("com.android.systemui.user.domain.interactor.GuestUserInteractor")
+                .getDeclaredMethod("isDeviceAllowedToAddGuest")
+            hookWithId(isAllowedMethod, "is_allowed") { chain ->
                 // 获取应用上下文
-                Context context = (Context) chain.getThisObject().getClass()
-                        .getDeclaredField("applicationContext").get(chain.getThisObject());
+                val context = chain.thisObject.javaClass
+                    .getDeclaredField("applicationContext").get(chain.thisObject) as Context?
 
                 // 检查用户切换器是否启用
-                int userSwitcherEnabled = Settings.Global.getInt(
-                        context.getContentResolver(),
-                        "user_switcher_enabled",
-                        0
-                );
+                val userSwitcherEnabled = Settings.Global.getInt(
+                    context!!.contentResolver,
+                    "user_switcher_enabled",
+                    0
+                )
 
                 // 如果用户切换器被禁用，则不允许添加访客
                 if (userSwitcherEnabled == 0) {
-                    logger.debug("阻止自动添加访客用户 - 用户切换器已禁用");
-                    return false;
+                    logger.debug("阻止自动添加访客用户 - 用户切换器已禁用")
+                    return@hookWithId false
                 }
-                return chain.proceed();
-            });
+                chain.proceed()
+            }
 
-            logger.info("成功Hook访客用户交互器");
-        } catch (Throwable t) {
-            logger.error("Hook访客用户交互器失败", t);
+            logger.info("成功Hook访客用户交互器")
+        } catch (t: Throwable) {
+            logger.error("Hook访客用户交互器失败", t)
         }
     }
 }
