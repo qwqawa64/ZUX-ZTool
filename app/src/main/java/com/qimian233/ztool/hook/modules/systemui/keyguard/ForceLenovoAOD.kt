56008cc11f7d700b88bdee0d385eed9542d07969
WASDDestroy <3288211880@qq.com>
2026-08-10 19:34:36 +0800

    refactor: convert ForceLenovoAOD.java to Kotlin

--- app/src/main/java/com/qimian233/ztool/hook/modules/systemui/keyguard/ForceLenovoAOD.kt
+++ app/src/main/java/com/qimian233/ztool/hook/modules/systemui/keyguard/ForceLenovoAOD.kt
@@ -1,97 +1,93 @@
-package com.qimian233.ztool.hook.modules.systemui.keyguard;
+package com.qimian233.ztool.hook.modules.systemui.keyguard
 
-import android.annotation.SuppressLint;
-import android.content.ContentResolver;
-import android.content.Context;
-
-import com.qimian233.ztool.data.keys.ScopeKeys;
-import com.qimian233.ztool.hook.base.AppHookModule;
-
-import io.github.libxposed.api.XposedModuleInterface;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
+import android.annotation.SuppressLint
+import android.content.ContentResolver
+import android.content.Context
+import android.provider.Settings
+import com.qimian233.ztool.data.keys.PreferenceKeys
+import com.qimian233.ztool.data.keys.ScopeKeys
+import com.qimian233.ztool.hook.base.AppHookModule
+import io.github.libxposed.api.XposedModuleInterface.PackageLoadedParam
+import java.lang.reflect.Constructor
 
 @SuppressLint("PrivateApi")
-public class ForceLenovoAOD extends AppHookModule {
+class ForceLenovoAOD : AppHookModule() {
+    override fun getModuleName(): String = PreferenceKeys.FORCE_LENOVO_AOD.name
 
-    private static final String TAG = "ForceLenovoAOD";
+    override fun getTargetPackages(): Array<String> = arrayOf(ScopeKeys.SYSTEM_UI.packageName)
 
-    public ForceLenovoAOD() {}
 
-    @Override
-    public String getModuleName() {
-        return TAG;
-    }
-
-    @Override
-    public String[] getTargetPackages() {return new String[]{ScopeKeys.SYSTEM_UI.packageName};}
-
-    private static final String ZUI_DOZE_TRIGGERS_CLASS = "com.android.systemui.doze.ZuiDozeTriggers";
-
-    @Override
-    public void handleLoadPackage(XposedModuleInterface.PackageLoadedParam param) throws Throwable {
-        ClassLoader classLoader = param.getDefaultClassLoader();
-        String packageName = param.getPackageName();
-        if (!ScopeKeys.SYSTEM_UI.packageName.equals(packageName)) {
-            return;
-        }
-
+    @Throws(Throwable::class)
+    override fun handleLoadPackage(param: PackageLoadedParam) {
+        val classLoader = param.defaultClassLoader
         // 直接设置mIsGoingToStartAOD字段
-        hookZuiDozeTriggers(classLoader);
+        hookZuiDozeTriggers(classLoader)
         // 额外确保AOD相关检查通过
-        hookAODChecks();
+        hookAODChecks()
     }
 
-    private void hookZuiDozeTriggers(ClassLoader classLoader) {
+    private fun hookZuiDozeTriggers(classLoader: ClassLoader) {
         try {
             // Hook ZuiDozeTriggers的构造函数，确保实例创建后立即设置标志
-            Constructor<?> ctor = classLoader.loadClass(ZUI_DOZE_TRIGGERS_CLASS)
-                    .getDeclaredConstructor(
-                            classLoader.loadClass("com.android.systemui.doze.DozeTriggers"),
-                            Context.class);
-            hookWithId(ctor, "ctor", chain -> {
-                chain.proceed();
+            val ctor: Constructor<*> = classLoader.loadClass(ZUI_DOZE_TRIGGERS_CLASS)
+                .getDeclaredConstructor(
+                    classLoader.loadClass("com.android.systemui.doze.DozeTriggers"),
+                    Context::class.java
+                )
+            hookWithId(ctor, "ctor") { chain ->
+                chain.proceed()
                 // 在构造函数执行后，立即设置AOD启动标志
-                chain.getThisObject().getClass().getDeclaredField("mIsGoingToStartAOD")
-                        .setBoolean(chain.getThisObject(), true);
-                logger.debug("ZuiDozeTriggers constructed, forced mIsGoingToStartAOD = true");
-                return null;
-            });
-        } catch (Throwable t) {
-            logger.error("Failed to hook ZuiDozeTriggers: ", t);
+                chain.thisObject.javaClass.getDeclaredField("mIsGoingToStartAOD")
+                    .setBoolean(chain.thisObject, true)
+                logger.debug("ZuiDozeTriggers constructed, forced mIsGoingToStartAOD = true")
+                null
+            }
+        } catch (t: Throwable) {
+            logger.error("Failed to hook ZuiDozeTriggers: ", t)
         }
     }
 
-    private void hookAODChecks() {
+    private fun hookAODChecks() {
         try {
             // Hook SystemProperties检查
-            Method getIntMethod = Class.forName("android.os.SystemProperties")
-                    .getDeclaredMethod("getInt", String.class, int.class);
-            hookWithId(getIntMethod, "get_int", chain -> {
-                String key = (String) chain.getArg(0);
-                if ("ro.config.aod.support".equals(key)) {
-                    logger.debug("Bypassed ro.config.aod.support check");
-                    return 1; // 强制返回支持AOD
+            val getIntMethod = Class.forName("android.os.SystemProperties")
+                .getDeclaredMethod("getInt", String::class.java, Int::class.javaPrimitiveType)
+            hookWithId(getIntMethod, "get_int") { chain ->
+                val key = chain.args[0] as String
+                if ("ro.config.aod.support" == key) {
+                    logger.debug("Bypassed ro.config.aod.support check")
+                    return@hookWithId 1 // 强制返回支持AOD
                 }
-                return chain.proceed();
-            });
+                chain.proceed()
+            }
 
             // Hook AOD设置检查
-            Method getIntForUserMethod = android.provider.Settings.System.class
-                    .getDeclaredMethod("getIntForUser",
-                            ContentResolver.class, String.class, int.class, int.class);
-            hookWithId(getIntForUserMethod, "get_int_for_user", chain -> {
-                String setting = (String) chain.getArg(1);
-                if ("always_on_display".equals(setting)) {
-                    logger.debug("Bypassed always_on_display setting check");
-                    return 1;
+            @SuppressLint("DiscouragedPrivateApi") val getIntForUserMethod =
+                Settings.System::class.java
+                    .getDeclaredMethod(
+                        "getIntForUser",
+                        ContentResolver::class.java,
+                        String::class.java,
+                        Int::class.javaPrimitiveType,
+                        Int::class.javaPrimitiveType
+                    )
+            hookWithId(
+                getIntForUserMethod,
+                "get_int_for_user"
+            ) { chain ->
+                val setting = chain.args[1] as String
+                if ("always_on_display" == setting) {
+                    logger.debug("Bypassed always_on_display setting check")
+                    return@hookWithId 1
                 }
-                return chain.proceed();
-            });
-
-        } catch (Exception t) {
-            logger.error("Failed to hook AOD checks: ", t);
+                chain.proceed()
+            }
+        } catch (t: Exception) {
+            logger.error("Failed to hook AOD checks: ", t)
         }
+    }
+
+    companion object {
+        private const val ZUI_DOZE_TRIGGERS_CLASS = "com.android.systemui.doze.ZuiDozeTriggers"
     }
 }
